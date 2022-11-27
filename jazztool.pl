d9b65ae5335e2123190b7d9ab640fa233ab0b628
Gene Boggs <gene@ology.net>
2022-11-27 15:20:45 -0800

    use midijs

--- jazztool.pl
+++ jazztool.pl
@@ -51,6 +51,7 @@ __DATA__
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script type='text/javascript' src='/midi.js'></script>
     <title><%= title %></title>
     <style>
       .padpage {
