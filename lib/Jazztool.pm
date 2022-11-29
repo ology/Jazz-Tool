c9a4046318b665e64e2922407a34670e0e641642
Gene Boggs <gene@ology.net>
2022-11-28 16:25:30 -0800

    *Just* chord names please

--- lib/Jazztool.pm
+++ lib/Jazztool.pm
@@ -139,7 +139,8 @@ sub chords {
             push @spec, [ $self->drummer->whole, @notes ];
         }
 
-        push @msgs, sprintf '%*d. %13s', length($self->phrases), $n + 1, $names;
+        push @msgs, $names;
+#        push @msgs, sprintf '%*d. %13s', length($self->phrases), $n + 1, $names;
 #        push @msgs, sprintf '%*d. %13s: %s',
 #            length($self->phrases), $n + 1, $names, ddc(\@spec);
 
